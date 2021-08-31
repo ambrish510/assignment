10f9e0a3a1cd009754ad91af9f728be7940f4b34
patil ambrish <33837950+ambrish510@users.noreply.github.com>
2021-08-31 21:30:55 +0530

    Added all the queries

--- src/main/java/upgrad/CRUDHelper.java
+++ src/main/java/upgrad/CRUDHelper.java
@@ -1,18 +1,75 @@
 package upgrad;
 
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Accumulators;
 import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
+import sun.net.www.content.text.PlainTextInputStream;
 
+import javax.swing.event.DocumentEvent;
+import javax.swing.plaf.basic.BasicColorChooserUI;
+import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.Arrays;
 
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Projections.excludeId;
 import static com.mongodb.client.model.Projections.fields;
 
 public class CRUDHelper {
+
+
+    /**
+     * Import the Data into MongoDB from MySQL remote server
+     */
+
+    public static void importDataIntoMongoDB(
+            Connection sqlConnection,
+            Statement statement,
+            String TableName,
+            String Category,
+            MongoCollection<Document> collection) throws SQLException {
+
+        try{
+            String sql = "select * from " + TableName;
+            ResultSet resultSet = statement.executeQuery(sql);
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            int colCount = resultSetMetaData.getColumnCount();
+
+            System.out.println("############### Importing the data from MySQL table " + TableName + " into Mongo Collection Products Started ###############");
+            System.out.println();
+
+            //Inserting the table records into Mongo DB collection as document one by one
+            while(resultSet.next()){
+                Document doc = new Document();
+                for(int i=1;i<=colCount;i++){
+                    doc.append(resultSetMetaData.getColumnLabel(i),resultSet.getString(resultSetMetaData.getColumnLabel(i)))
+                            .append("Category",Category);
+                }
+
+                collection.insertOne(doc);
+                System.out.println("Document inserted successfully into Mongo Collection products with Product Id: " + doc);
+                System.out.println();
+            }
+
+            System.out.println("############### Importing the data from MySQL table " + TableName + " into Mongo Collection Products ended ###############");
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+        }
+       catch(SQLException e){
+            System.out.println("Got Exception.");
+            e.printStackTrace();
+
+       }
+
+    }
 
     /**
      * Display ALl products
@@ -21,6 +78,11 @@ public class CRUDHelper {
     public static void displayAllProducts(MongoCollection<Document> collection) {
         System.out.println("------ Displaying All Products ------");
         // Call printSingleCommonAttributes to display the attributes on the Screen
+        MongoCursor<Document> cursor = collection.find().cursor();
+        while(cursor.hasNext()){
+            Document doc = cursor.next();
+            PrintHelper.printSingleCommonAttributes(doc);
+        }
 
     }
 
@@ -31,6 +93,11 @@ public class CRUDHelper {
     public static void displayTop5Mobiles(MongoCollection<Document> collection) {
         System.out.println("------ Displaying Top 5 Mobiles ------");
         // Call printAllAttributes to display the attributes on the Screen
+        MongoCursor<Document> cursor = collection.find(eq("Category","Mobile")).limit(5).cursor();
+        while(cursor.hasNext()){
+            Document doc = cursor.next();
+            PrintHelper.printAllAttributes(doc);
+        }
     }
 
     /**
@@ -40,6 +107,11 @@ public class CRUDHelper {
     public static void displayCategoryOrderedProductsDescending(MongoCollection<Document> collection) {
         System.out.println("------ Displaying Products ordered by categories ------");
         // Call printAllAttributes to display the attributes on the Screen
+        MongoCursor<Document> cursor = collection.find().sort(Sorts.descending("Category")).projection(Projections.excludeId()).cursor();
+        while(cursor.hasNext()){
+            Document doc = cursor.next();
+            PrintHelper.printAllAttributes(doc);
+        }
     }
 
 
@@ -50,6 +122,19 @@ public class CRUDHelper {
     public static void displayProductCountByCategory(MongoCollection<Document> collection) {
         System.out.println("------ Displaying Product Count by categories ------");
         // Call printProductCountInCategory to display the attributes on the Screen
+        AggregateIterable<Document> Itr = collection.aggregate(
+                Arrays.asList(
+                        Aggregates.group("$Category", Accumulators.sum("count",1))
+                )
+        );
+
+        MongoCursor<Document> cursor = Itr.cursor();
+
+        while(cursor.hasNext()){
+            Document doc = cursor.next();
+            PrintHelper.printProductCountInCategory(doc
+            );
+        }
     }
 
     /**
@@ -59,5 +144,10 @@ public class CRUDHelper {
     public static void displayWiredHeadphones(MongoCollection<Document> collection) {
         System.out.println("------ Displaying Wired headphones ------");
         // Call printAllAttributes to display the attributes on the Screen
+        MongoCursor<Document> cursor = collection.find(and(eq("Category","Headphone"),eq("ConnectorType","Wired"))).cursor();
+        while(cursor.hasNext()){
+            Document doc = cursor.next();
+            PrintHelper.printAllAttributes(doc);
+        }
     }
 }
